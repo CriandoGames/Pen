ced245ee537b8ceea515a191dbffdf16cf7ba025
Carlos Castro <carloscastrogames@gmail.com>
2018-03-30 14:14:31 -0300

    Commit Sexta Feira 30-03-2018
    create pin Script and Base.

---
+++ Pen/Assets/_Game/Script/Pin/PinBase.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace _Game.Script.Pin
+{
+    public class PinBase : MonoBehaviour
+    {
+        [SerializeField] protected float speed = 20f;
+        protected Rigidbody2D rigidbody2;
+
+
+        protected virtual void Start()
+        {
+            rigidbody2 = GetComponent<Rigidbody2D>();
+        }
+
+        /// <summary>
+        /// Set Speed Pin
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetSpeed(float value)
+        {
+            this.speed = value;
+        }
+
+
+        protected virtual void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag.Equals("Ball"))
+            {
+                transform.SetParent(other.transform);
+            }
+        }
+    }
+}
---
+++ Pen/Assets/_Game/Script/Pin/PinScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+namespace _Game.Script.Pin
+{
+    public class PinScript : PinBase
+    {
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+
+        private void Update()
+        {
+            Shot();
+        }
+
+
+        private void Shot()
+        {
+            rigidbody2.MovePosition(rigidbody2.position + Vector2.up * speed * Time.deltaTime);
+        }
+
+
+        protected override void OnTriggerEnter2D(Collider2D other)
+        {
+            base.OnTriggerEnter2D(other);
+        }
+    }
+}
