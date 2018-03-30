25900464f7dc420024d8b8a71245ca11bc54a3a2
Carlos Castro <carloscastrogames@gmail.com>
2018-03-30 13:52:12 -0300

    Create Scripts
    Create Script - Base and Skill

---
+++ Pen/Assets/_Game/Script/Cicles/Skill01Script.cs
@@ -0,0 +1,18 @@
+﻿using _Game.Script.Common;
+
+namespace _Game.Script.Cicles
+{
+    public class Skill01Script : RotateScript
+    {
+        private void Update()
+        {
+            this.Rotate();
+        }
+
+
+        public override void Rotate()
+        {
+            base.Rotate();
+        }
+    }
+}
---
+++ Pen/Assets/_Game/Script/Common/RotateBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace _Game.Script.Common
+{
+    public class RotateScript : MonoBehaviour
+    {
+        
+        [SerializeField]
+        protected float Speed;
+
+
+
+
+        
+
+          
+
+        public virtual void Rotate()
+        {
+            transform.Rotate(0f,0f,Speed*Time.deltaTime);
+        }
+
+
+    }
+}
